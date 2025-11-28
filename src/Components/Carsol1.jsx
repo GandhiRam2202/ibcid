7ccc7548aba72d2f8207c21192be2587b0af62a2
GandhiRam2202 <gandhiramparthi.com>
2025-11-29 01:28:09 +0530

    carsol update

---
+++ src/Components/Carsol1.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import img0 from "/images/123.jpg";
+import img1 from "/images/1231.jpg";
+import img2 from "/images/132.jpg";
+import img3 from "/images/1321.jpg";
+import img4 from "/images/143.jpg";
+
+export default function ImageCarousel() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    pauseOnHover: true,
+  };
+
+  const slides = [
+    { img: img0, text: "Welcome to Our Website" },
+    { img: img1, text: "Best Services in the City" },
+    { img: img2, text: "Professional & Trusted Team" },
+    { img: img3, text: "We Deliver Quality Work" },
+    { img: img4, text: "We Deliver Quality Work" },
+  ];
+
+  return (
+    <div className="container">
+      <Slider {...settings}>
+        {slides.map((slide, i) => (
+          <div key={i} style={{ position: "relative" }}>
+            <img
+              src={slide.img}
+              alt={`slide-${i}`}
+              style={{
+                width: "100%",
+                height: "500px",
+                
+                borderRadius: "12px",
+              }}
+            />
+
+            {/* Text Overlay */}
+           
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import Carsol from './Carsol'
+import Carsol1 from './Carsol1'
 
 const Homepage = () => {
     return (
         <>
         
-      
+    <div className="d-none mt-2 d-lg-block">
         <Carsol/>
-        <Carsol/>
-        <Carsol/>
-        <Carsol/>
-        <Carsol/>
-        <Carsol/>
-        <Carsol/>
-        <Carsol/>
-        <Carsol/>
-        <Carsol/>
-        <Carsol/>
-        <div>
-            home
+    </div>
+    <div className=" mt-2 d-lg-none d-sm-block">
+        <Carsol1/>
+    </div>
+
+       
+        <div className='mt-4 container'>
+            <h1 className='text'>About IBCID</h1>
         </div>
         </>
     );
