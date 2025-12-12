d3514dc17cb96c3e27ad27accb1e89217d781a40
GandhiRam2202 <gandhiramparthi.com>
2025-12-12 23:06:42 +0530

    update

--- src/Components/Personal.jsx
+++ src/Components/Personal.jsx
@@ -91,24 +91,7 @@ const Personal = () => {
 
                                 </ul>
                             </div>
-                            <div className="col-sm-6 d-none d-lg-block">
-                                <Link to="/Contactus" className="btn btn-danger">
-                                    Contact Us
-                                </Link>
-                            </div>
-                            <div className="col-sm-6 text-center d-block d-lg-none">
-                                <Link to="/Contactus" className="btn btn-danger">
-                                    Contact Us
-                                </Link>
-                            </div>
-                            <div className="col-sm-6 text-center">
-                                <div className="row">
-                                    <div className="col-3 text-danger mt-2 p-0" onClick={handleOpenMap}><i className="bi bi-geo-alt-fill"></i></div>
-                                    <div className="col-3 text-danger mt-2 p-0" onClick={handleCall}><i className="bi bi-telephone-fill"></i></div>
-                                    <div className="col-3 text-danger mt-2 p-0" onClick={handleMail}><i className="bi bi-envelope-fill"></i></div>
-                                    <div className="col-3 text-danger mt-2 p-0" onClick={handleWap}><i className="bi bi-whatsapp"></i></div>
-                                </div>
-                            </div>
+
                         </div>
 
                         <p className='mt-4 fw-bold'>⭐ Tracking People – Find Anyone, Anytime With 100% Confidentiality</p>
@@ -138,7 +121,164 @@ const Personal = () => {
                 </div>
 
             </div>
-                <TextCar/>
+            <TextCar />
+
+            <div className="container">
+                <div className="col-sm-12">
+                    <div className="row">
+                        <div className="col-sm-12 p-0 col-lg-6 text-warp">
+                            <h1 className='m-4 text-danger'>
+                                Divorce
+                            </h1>
+                            <p className='fw-bold'>🔥 Divorce Investigation – Get the Truth You Deserve Before Making Life Decisions</p>
+
+
+                            <p>Divorce is one of the most emotionally stressful phases in life. When doubts,
+                                lies, or hidden activities start affecting your peace, <span className='fw-bold text-danger'>IBCI Detective </span>provides clear,
+                                reliable, and confidential evidence to help you make the right decision with confidence.</p>
+
+                            <p> Our Divorce Investigation Service is designed to uncover the truth discreetly, legally,
+                                and professionally — ensuring you have the facts you need for your personal clarity or legal proceedings.</p>
+
+                            <p className='fw-bold mt-2 mb-1'> 🔍 Why Do Clients Need Divorce Investigation Services? </p>
+                            <li className="d-flex align-items-center mb-2">
+                                ✔ Suspected extra-marital affairs / cheating</li>
+                            <li className="d-flex align-items-center mb-2">
+                                ✔ Sudden change in partner’s behaviour or routine</li>
+                            <li className="d-flex align-items-center mb-2">
+                                ✔ Unexplained expenses or secret financial activities</li>
+                            <li className="d-flex align-items-center mb-2">
+                                ✔ Lack of transparency, frequent lies, or late-night outings</li>
+                            <li className="d-flex align-items-center mb-2">
+                                ✔ Doubts during child custody or maintenance cases</li>
+                            <li className="d-flex align-items-center mb-2">
+                                ✔ Safety concerns or emotional harassment</li>
+                            <li className="d-flex align-items-center mb-2">
+                                ✔ Suspicion of hidden assets or property</li>
+
+
+                        </div>
+                        <div className="col-sm-12 p-0 col-lg-6 d-none d-lg-block">
+
+
+                            <div className="frame-outline">
+                                <div className="frame-bg d-flex justify-content-center align-items-center">
+                                    <img
+                                        src="/images/divo.png"
+                                        alt=""
+                                        className="frame-img" style={{ height: '550px' }}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-12 p-0 col-lg-6 d-block d-lg-none">
+
+
+                            <div className="frame-outline">
+                                <div className="frame-bg d-flex justify-content-center align-items-center">
+                                    <img
+                                        src="/images/divo.png"
+                                        alt=""
+                                        className="frame-img" style={{ height: '400px' }}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div className="container mt-2">
+                <div className="row">
+                    <div className="col-sm-12 text-center d-none d-lg-block p-0 col-lg-6">
+                        <div className="frame-outline">
+                            <div className="frame-bg1 d-flex justify-content-center align-items-center">
+                                <img
+                                    src="/images/mat1.png"
+                                    alt=""
+                                    className="frame-img" style={{ height: '600px' }}
+                                />
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className="col-sm-12 text-center d-block d-lg-none p-0 col-lg-6">
+                        <div className="frame-outline">
+                            <div className="frame-bg1 d-flex  justify-content-center align-items-center">
+                                <img
+                                    src="/images/mat1.png"
+                                    alt=""
+                                    className="frame-img" style={{ height: '500px' }}
+                                />
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <div className="col-sm-12 text-warp col-lg-6 d-flex-column mt-2 justify-content-center align-items-center">
+                        <h1 className='text-danger'>Matrimonial Investigation</h1>
+
+                        <p className='fw-bold'>Pre-Matrimonial Investigation</p>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Real background & lifestyle</li>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Job, income & employment status</li>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Family background & behaviour</li>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Habits: smoking, drinking, gambling</li>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Past relationships or affairs</li>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Criminal record & legal issues</li>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Personal character verification</li>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Social media & digital footprint</li>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Financial stability</li>
+                        <li className="d-flex align-items-center mb-2">
+                           ✔ Hidden debts or loans</li>
+
+
+
+                        <p className='fw-bold mt-3'>Post-Matrimonial Investigation</p>
+                        <li className="d-flex align-items-center mb-2">
+                            ✔ Extra-marital affair verification</li>
+                        <li className="d-flex align-items-center mb-2">
+                            ✔ Behaviour & movement tracking</li>
+                        <li className="d-flex align-items-center mb-2">
+                            ✔ Late-night activities monitoring</li>
+                        <li className="d-flex align-items-center mb-2">
+                            ✔ Communication & social circle analysis</li>
+                        <li className="d-flex align-items-center mb-2">
+                            ✔ Suspicious behaviour documentation</li>
+                        <li className="d-flex align-items-center mb-2">
+                            ✔ Evidence for legal support</li>
+
+                    </div>
+
+                </div>
+                 <p className='fw-bold text-center mt-2'>💍 Matrimonial Investigation – Ensure Trust Before & After Marriage</p>
+
+                        <p className='text-center'>Marriage is a lifelong commitment — but today, trust can’t be built on assumptions.</p>
+                            <p className='text-center'>Our <b>Matrimonial Investigation Services</b> help you verify the truth, uncover hidden details,
+                            and protect your future from risks and surprises.</p>
+                        <p className='text-center m-0'>Whether it’s Pre-Matrimonial Verification or Post-Matrimonial Investigation, we provide accurate,
+                            confidential, and court-usable evidence.</p>
+
+
+            </div>
+
+
+
             <div className="container-fluid p-0"
                 style={{
                     backgroundImage: "url('/images/con.png')",
@@ -184,7 +324,7 @@ const Personal = () => {
                     </div>
                 </div>
             </div>
-        
+
         </>
     );
 };
