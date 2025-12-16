60412beacdcda9971c92fc1b4ec27cb59a6e7b2d
GandhiRam2202 <gandhiramparthi.com>
2025-12-16 18:27:32 +0530

    update

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -6,6 +6,8 @@ import TextCar from './TextCar';
 import { Link } from 'react-router-dom';
 import BgImage from './BgImage';
 import Trust from './Trust';
+import { handleCall, handleOpenMap, handleWap, handleMail } from './Contact';
+import ContactForm from './ContactForm';
 
 
 
@@ -20,7 +22,7 @@ const Homepage = () => {
 
 
 
-          <Bgimg/>
+            <Bgimg />
 
 
 
@@ -116,59 +118,144 @@ const Homepage = () => {
                     <div className="row p-0 m-0 justify-content-center">
 
 
-                       
-                                <div className="col-sm-12 col-lg-3 d-flex text-center justify-content-center p-2 mb-3 fw-bold">
-                                    <div className="card crd1" style={{ width: "18rem" }}>
-                                        
-                                        <h4 className="card-title mt-4 text-white">Personal Sevices</h4>
-                                        <div className="card-body p-0">
-                                            <div className='border border-danger'>
-                                           <Link className="text-decoration-none text-white" to='/Contactus'>Enquiry Now</Link></div>
-                                        </div>
-                                    </div>
-                                </div>
 
-                                <div className="col-sm-12 col-lg-3 d-flex text-center justify-content-center p-2 mb-3 fw-bold">
-                                    <div className="card crd1" style={{ width: "18rem" }}>
-                                        
-                                        <h4 className="card-title mt-4 text-white">Corporate Sevices</h4>
-                                        <div className="card-body p-0">
-                                            <div className='border border-danger'>
-                                           <Link className="text-decoration-none text-white" to='/Contactus'>Enquiry Now</Link></div>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div className="col-sm-12 col-lg-3 d-flex text-center justify-content-center p-2 mb-3 fw-bold">
+                            <div className="card crd1" style={{ width: "18rem" }}>
 
-                                <div className="col-sm-12 col-lg-3 d-flex text-center justify-content-center p-2 mb-3 fw-bold">
-                                    <div className="card crd1" style={{ width: "18rem" }}>
-                                        
-                                        <h4 className="card-title mt-4 text-white">Legal Sevices</h4>
-                                        <div className="card-body p-0">
-                                            <div className='border border-danger'>
-                                           <Link className="text-decoration-none text-white" to='/Contactus'>Enquiry Now</Link></div>
-                                        </div>
-                                    </div>
+                                <h4 className="card-title mt-4 text-white">Personal Sevices</h4>
+                                <div className="card-body p-0">
+                                    <div className='border border-danger'>
+                                        <Link className="text-decoration-none text-white" to='/Contactus'>Enquiry Now</Link></div>
                                 </div>
-                         
-                                <div className="col-sm-12 col-lg-3 d-flex text-center justify-content-center p-2 mb-3 fw-bold">
-                                    <div className="card crd1" style={{ width: "18rem" }}>
-                                        
-                                        <h4 className="card-title mt-4 text-white">Special Sevices</h4>
-                                        <div className="card-body p-0">
-                                            <div className='border border-danger'>
-                                           <Link className="text-decoration-none text-white" to='/Contactus'>Enquiry Now</Link></div>
-                                        </div>
-                                    </div>
+                            </div>
+                        </div>
+
+                        <div className="col-sm-12 col-lg-3 d-flex text-center justify-content-center p-2 mb-3 fw-bold">
+                            <div className="card crd1" style={{ width: "18rem" }}>
+
+                                <h4 className="card-title mt-4 text-white">Corporate Sevices</h4>
+                                <div className="card-body p-0">
+                                    <div className='border border-danger'>
+                                        <Link className="text-decoration-none text-white" to='/Contactus'>Enquiry Now</Link></div>
                                 </div>
+                            </div>
+                        </div>
+
+                        <div className="col-sm-12 col-lg-3 d-flex text-center justify-content-center p-2 mb-3 fw-bold">
+                            <div className="card crd1" style={{ width: "18rem" }}>
+
+                                <h4 className="card-title mt-4 text-white">Legal Sevices</h4>
+                                <div className="card-body p-0">
+                                    <div className='border border-danger'>
+                                        <Link className="text-decoration-none text-white" to='/Contactus'>Enquiry Now</Link></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-sm-12 col-lg-3 d-flex text-center justify-content-center p-2 mb-3 fw-bold">
+                            <div className="card crd1" style={{ width: "18rem" }}>
+
+                                <h4 className="card-title mt-4 text-white">Special Sevices</h4>
+                                <div className="card-body p-0">
+                                    <div className='border border-danger'>
+                                        <Link className="text-decoration-none text-white" to='/Contactus'>Enquiry Now</Link></div>
+                                </div>
+                            </div>
+                        </div>
 
 
 
                     </div>
                 </div>
             </div>
-<div className="container-fluid p-0">
-            <TextCar /></div>
-            <Trust/>
+
+     <div className="container border border-1 shadow-lg" >
+        <div className="col-sm-12">
+          <div className="row">
+            <div className="col-sm-12 p-0 col-lg-6">
+
+
+              <div
+                className="container-fluid p-0"
+                style={{
+                  backgroundImage: "url('/images/con.png')",
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  height: "100%",
+                }}
+              >
+                <div
+                  className="container-fluid p-0 h-100 d-flex justify-content-center align-items-center"
+                  style={{
+                    background: "rgba(0, 0, 0, 0.5)",
+                  }}
+                >
+                  <div className="text-center">
+                    <div
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: "2rem",
+                        color: "white",
+                      }}
+                    >
+                     CONTACT US
+                    </div>
+
+                    <div
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: "1.2rem",
+                        color: "white",
+                      }}
+                    >
+                      Need Assistance?
+                    </div>
+                    <div className="d-flex justify-content-center text-center text-white mt-4 p-0" onClick={handleCall}>
+                      <div className="col-sm-12 bg rounded">
+                        <i className="p-0 bi bi-telephone-fill m-1">
+                          (+91)9043396037
+                        </i>
+                      </div>
+                    </div>
+
+
+                    <div className="container mt-3 text-white">
+                      <div className="col-sm-12">
+                        <div className="row text-center">
+                          <div className="col-3 p-0" onClick={handleOpenMap}><i className="biho bi bi-geo-alt-fill"></i></div>
+                          <div className="col-3 p-0" onClick={handleCall}><i className="bi biho bi-telephone-fill"></i></div>
+                          <div className="col-3 p-0" onClick={handleMail}><i className="bi biho bi-envelope-fill"></i></div>
+                          <div className="col-3 p-0" onClick={handleWap}><i className="bi biho bi-whatsapp"></i></div>
+                        </div>
+                      </div>
+                    </div>
+
+
+
+
+                  </div>
+                </div>
+              </div >
+
+
+
+
+
+            </div>
+           
+            <div className="col-sm-12 col-lg-6">
+              <ContactForm/>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+            <div className="container-fluid p-0">
+                <TextCar /></div>
+            <Trust />
 
             <BgImage />
 
